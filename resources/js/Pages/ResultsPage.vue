9297b8942be15e889b6b4c7385a0875b42840749
ahmedmoud <ahmedmoud@ymail.com>
2024-07-02 20:02:07 +0300

    icons

--- resources/js/Pages/ResultsPage.vue
+++ resources/js/Pages/ResultsPage.vue
@@ -289,12 +289,12 @@
                                                     </div>
                                                     <div class="col-md-2 row">
                                                         <div>
-                                                            <span style="font-size: 1vw; margin-left: -50px; color: #000;"
+                                                            <span style="font-size: .85vw; margin-left: -50px; color: #000;"
                                                                   class="text-nowrap">{{
                                                                     vehicle.supplier.company
                                                                 }}</span>
                                                         </div>
-                                                        <div style="margin-left: -50px">
+                                                        <div style="margin-left: -50px; margin-top: -10px;">
                                                             <small><a class="cursor-pointer text-primary"
                                                                       href="javascript:void(0);"
                                                                       @click="openRentalTerms(vehicle)">Rental&nbsp;Terms</a></small>
