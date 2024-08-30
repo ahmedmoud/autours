2877ece9643348dbeadc67714d6a8e250caa4828
ahmedmoud <ahmedmoud@ymail.com>
2024-08-30 23:44:47 +0300

    a

--- resources/views/email/subscribe-emails/be-supplier.blade.php
+++ resources/views/email/subscribe-emails/be-supplier.blade.php
@@ -3,7 +3,7 @@ Hi Dear,<br>
 Thank you for registering your company with www.autours.net ! We’re excited to welcome your business to our community.<br>
 To complete the registration process and activate your company’s account, please click the link below:<br>
 Confirm Registration<br>
-If you did not register your company with <a href="http://staging.autours.net/">Autours Suppliers </a>  or received this email in error, you can safely ignore it.<br>
+If you did not register your company with <a href="http://staging.autours.net/register?user_type=supplier">Autours Suppliers </a>  or received this email in error, you can safely ignore it.<br>
 If you have any questions or need further assistance, please feel free to contact us at sales@autours.net <br>
 We look forward to working with you!<br>
 Best regards,<br>
