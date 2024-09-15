22fdafa3e6cd49c4a6cfe60187f2ce8a54fde3f4
ahmedmoud <ahmedmoud@ymail.com>
2024-09-16 00:58:26 +0300

    a

--- resources/js/Pages/ResultsPage.vue
+++ resources/js/Pages/ResultsPage.vue
@@ -447,7 +447,7 @@
                                             <div class="row rounded-2 mb-3 py-1 "
                                                  style="background: #edecec; width: 76.5%;">
                                                 <div class="d-supplier">
-                                                    <div class="mt-2 d-img" style="width: 15%; height: 60px; ">
+                                                    <div class="mt-2 d-img" style="width: 12%; height: 60px; ">
                                                         <img :src="'img/' + vehicle.supplier.logo" height="50"
                                                              width="80" alt=""/>
                                                     </div>
