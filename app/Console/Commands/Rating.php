8e541a7db66c074ec379f4fa7b43496c7b22fa7b
ahmedmoud <ahmedmoud@ymail.com>
2024-09-30 03:20:13 +0300

    a

---
+++ app/Console/Commands/Rating.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\RentalRateRequestMail;
+use App\Models\Rental;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class Rating extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:rating';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $rentalsHasNoReview = Rental::query()->whereDate('end_date','>=', Carbon::now()->subDays(4)->toDateString())->with(['customer'])->get();
+        foreach ($rentalsHasNoReview as $rentalHasNoReview) {
+            $rentalHasNoReview->review_form_link = url('rentals/rate?id='. $rentalHasNoReview->id .'&key=' .encrypt(base64_encode($rentalHasNoReview->customer->email .','. Carbon::now()->addDays(2)->toDateString()), env('APP_KEY')));
+            Mail::to($rentalHasNoReview->customer->email)->send(new RentalRateRequestMail\RentalRateRequestMail($rentalHasNoReview));
+
+        }
+    }
+}
