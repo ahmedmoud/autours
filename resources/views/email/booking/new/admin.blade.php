86edbedfd20ecd1313fd8e6530246fc1ac9eca81
ahmedmoud <ahmedmoud@ymail.com>
2024-11-18 22:24:51 +0200

    a

--- resources/views/email/booking/new/admin.blade.php
+++ resources/views/email/booking/new/admin.blade.php
@@ -6,13 +6,14 @@ attention. Below are the details of the booking:<br>
 
 Booking Details:<br>
 
-Reservation Number: {{json_decode($body)->order_number}}<br>
-Customer's Name: {{json_decode($body)->customer->name}}<br>
-Car Model: {{json_decode($body)->vehicle->name}}<br>
-Pick-Up Date & Time: {{\Carbon\Carbon::parse(json_decode($body)->start_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->start_time)->toTimeString()}}<br>
-Drop-off Date and Time: {{\Carbon\Carbon::parse(json_decode($body)->end_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->end_time)->toTimeString()}}<br>
-Pick-Up Location: {{json_decode($body)->branch->adresse}}<br>
-Rental Amount: {{( (json_decode($body)->supplier_price * json_decode($body)->profit_margin ) / 100) . ' ' . json_decode($body)->currency}}<br>
+- Reservation Number: {{json_decode($body)->order_number}}<br>
+- Customer's Name: {{json_decode($body)->customer->name}}<br>
+- Car Model: {{json_decode($body)->vehicle->name}}<br>
+- Pick-Up Date & Time: {{\Carbon\Carbon::parse(json_decode($body)->start_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->start_time)->toTimeString()}}<br>
+- Drop-off Date and Time: {{\Carbon\Carbon::parse(json_decode($body)->end_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->end_time)->toTimeString()}}<br>
+- Pick-Up Location: {{json_decode($body)->branch->adresse}}<br>
+- Profit : {{( (json_decode($body)->supplier_price * json_decode($body)->profit_margin ) / 100) . ' ' . json_decode($body)->currency}}<br>
+- Amount :  {{ ceil( json_decode($body)->supplier_price +  ( json_decode($body)->supplier_price * (json_decode($body)->profit / 100) ) ) . ' ' . json_decode($body)->currency  }}<br>
 Action Required:<br>
 <br><br>
 Payment Information:<br>
