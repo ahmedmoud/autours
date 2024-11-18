02bcbe3e51bd57829a7d48d640751e6e7ce414bd
ahmedmoud <ahmedmoud@ymail.com>
2024-11-18 22:54:47 +0200

    a

--- resources/views/email/booking/new/admin.blade.php
+++ resources/views/email/booking/new/admin.blade.php
@@ -12,9 +12,12 @@ Booking Details:<br>
 - Pick-Up Date & Time: {{\Carbon\Carbon::parse(json_decode($body)->start_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->start_time)->toTimeString()}}<br>
 - Drop-off Date and Time: {{\Carbon\Carbon::parse(json_decode($body)->end_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->end_time)->toTimeString()}}<br>
 - Pick-Up Location: {{json_decode($body)->branch->adresse}}<br>
-- Profit Amount: {{ json_decode($body)->price - json_decode($body)->supplier_price   . ' ' . json_decode($body)->currency }}<br>
+
+- Total Amount :  {{ ceil( json_decode($body)->price)  . ' ' . json_decode($body)->currency }}<br>
+- Supplier Amount :  {{ ceil( json_decode($body)->price)  . ' ' . json_decode($body)->currency }}<br>
 - Profit Percentage: {{ json_decode($body)->profit_margin . '%' }}<br>
-- Amount :  {{ ceil( json_decode($body)->price)  . ' ' . json_decode($body)->currency }}<br>
+- Profit Amount: {{ json_decode($body)->price - json_decode($body)->supplier_price   . ' ' . json_decode($body)->currency }}<br>
+
 Action Required:<br>
 <br><br>
 Payment Information:<br>
