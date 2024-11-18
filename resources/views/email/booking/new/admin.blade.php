a44f21a51647a0029258cc190b4e014393a47551
ahmedmoud <ahmedmoud@ymail.com>
2024-11-18 22:51:06 +0200

    a

--- resources/views/email/booking/new/admin.blade.php
+++ resources/views/email/booking/new/admin.blade.php
@@ -13,7 +13,7 @@ Booking Details:<br>
 - Drop-off Date and Time: {{\Carbon\Carbon::parse(json_decode($body)->end_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->end_time)->toTimeString()}}<br>
 - Pick-Up Location: {{json_decode($body)->branch->adresse}}<br>
 - Profit Amount: {{ json_decode($body)->price - json_decode($body)->supplier_price   . ' ' . json_decode($body)->currency }}<br>
-- Profit Percentage: {{ json_decode($body)->profilt_margin . '%' }}<br>
+- Profit Percentage: {{ json_decode($body)->profit_margin . '%' }}<br>
 - Amount :  {{ ceil( json_decode($body)->price)  . ' ' . json_decode($body)->currency }}<br>
 Action Required:<br>
 <br><br>
