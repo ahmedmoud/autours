79226e3b075da3ec61bf6c4e51fab672c3cd30a9
ahmedmoud <ahmedmoud@ymail.com>
2024-11-18 22:49:50 +0200

    a

--- resources/views/email/booking/new/admin.blade.php
+++ resources/views/email/booking/new/admin.blade.php
@@ -12,8 +12,9 @@ Booking Details:<br>
 - Pick-Up Date & Time: {{\Carbon\Carbon::parse(json_decode($body)->start_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->start_time)->toTimeString()}}<br>
 - Drop-off Date and Time: {{\Carbon\Carbon::parse(json_decode($body)->end_date)->toDateString()}} {{\Carbon\Carbon::parse(json_decode($body)->end_time)->toTimeString()}}<br>
 - Pick-Up Location: {{json_decode($body)->branch->adresse}}<br>
-- Profit : {{ json_decode($body)->price - json_decode($body)->supplier_price   . ' ' . json_decode($body)->currency }}<br>
-- Amount :  {{ ceil( json_decode($body)->price) }}<br>
+- Profit Amount: {{ json_decode($body)->price - json_decode($body)->supplier_price   . ' ' . json_decode($body)->currency }}<br>
+- Profit Percentage: {{ json_decode($body)->profilt_margin . '%' }}<br>
+- Amount :  {{ ceil( json_decode($body)->price)  . ' ' . json_decode($body)->currency }}<br>
 Action Required:<br>
 <br><br>
 Payment Information:<br>
