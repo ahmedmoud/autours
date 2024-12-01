afef216eb66c8eb6fe6f50f99974629f8d6ae7c2
ahmedmoud <ahmedmoud@ymail.com>
2024-12-01 23:05:06 +0200

    a

--- resources/views/email/booking/new/admin.blade.php
+++ resources/views/email/booking/new/admin.blade.php
@@ -16,7 +16,7 @@ Booking Details:<br>
 - Total Amount :  {{ round( json_decode($body)->price, 2)  . ' ' . json_decode($body)->currency }}<br>
 - Supplier Amount :  {{ ceil( json_decode($body)->supplier_price)  . ' ' . json_decode($body)->branch->currency }}<br>
 - Profit Percentage: {{ json_decode($body)->profit_margin . '%' }}<br>
-- Profit Amount: {{ json_decode($body)->price - ((json_decode($body)->price * json_decode($body)->profit_margin) / 100)  . ' ' . json_decode($body)->currency }}<br>
+- Profit Amount: {{ json_decode($body)->price - (json_decode($body)->price / ((json_decode($body)->profit_margin / 100) + 1) )  . ' ' . json_decode($body)->currency }}<br>
 
 Action Required:<br>
 <br><br>
