9c701f0f993f339aebdcc2bab7d18743135a2fe7
ahmedmoud <ahmedmoud@ymail.com>
2025-01-04 18:18:45 +0200

    a

---
+++ resources/js/Pages/WhereWeAre.vue
@@ -0,0 +1,187 @@
+<template>
+    <body>
+    <div id="wrapper">
+
+        <!-- header begin -->
+        <header-one/>
+        <!-- header close -->
+        <!-- content begin -->
+        <div class="no-bottom no-top zebra" id="content">
+            <div id="top"></div>
+            <div class="offset-3 mb-3">
+                <img src="/images/where_we_are/title.png">
+            </div>
+            <p style="font-size: 1.2vw;" class="mx-5"> A Journey through Eight Vibrant Destinations
+                At our car rental service, we believe that the journey is just as important as the destination. With our
+                presence in eight stunning
+                locations Egypt, Jordan, Saudi Arabia, Bahrain, Qatar, Kuwait, the UAE, and Oman we invite you to
+                explore the rich tapestry of
+                culture, history, and adventure that each of these countries has to offer. </p>
+            <img style="width: 99.5%; margin-left: .2%; height: 900px;" src="/images/where_we_are/banner.png"/>
+            <div class="row mt-5">
+
+
+                <div class="ml-5">
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> EGYPT: THE
+                            LAND OF THE PHARAOHS</h2>
+                        <p style="font-size: 20px;">
+                            Imagine embarking on an adventure in Egypt, where the ancient world meets modern life.
+                            Picture yourself driving along the Nile River,
+                            the lifeblood of this incredible nation, as you make your way to the iconic Pyramids of
+                            Giza. The sight of these magnificent structures
+                            against the backdrop of a golden sunset is something you will never forget. After that, head
+                            to Luxor, often referred to as the world’s
+                            greatest open-air museum, where you can explore the stunning temples of Karnak and the
+                            Valley of the Kings.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/4.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/5.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/6.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> SAUDI ARABIA: A BLEND OF TRADITION AND MODERNITY</h2>
+                        <p style="font-size: 20px;">
+                            Saudi Arabia offers a fascinating blend of ancient traditions and modern marvels. Drive through Riyadh and discover the historic Al
+                            Masmak Fortress, a symbol of the kingdom’s rich heritage. Your journey should also take you to Jeddah, where the stunning Corniche
+                            and the iconic King Fahd Fountain await. For a taste of luxury, explore the bustling souks and modern shopping centers that highlight
+                            the country’s rapid development.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/8.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/19.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/20.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> BAHRAIN: A JEWEL OF THE GULF</h2>
+                        <p style="font-size: 20px;">
+                            In Bahrain, a small but captivating island nation, immerse yourself in a mix of culture and heritage. Drive to the Bahrain Fort, a UNESCO
+                            World Heritage site, and learn about the rich history of this archipelago. Do not forget to visit the vibrant Manama Souq, where you
+                            can experience local flavors and buy unique handicrafts. The tree of life, a solitary tree thriving in the desert, is a must-see,
+                            surrounded by vast stretches of sand.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/24.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/22.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/23.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> QATAR: A MODERN MARVEL</h2>
+                        <p style="font-size: 20px;">
+                            Qatar is a country that beautifully juxtaposes tradition and modernity. In Doha, explore the dazzling Museum of Islamic Art, set
+                            against a stunning waterfront. Drive along the Corniche to enjoy views of the skyline, before heading to Souq Waqif, where the
+                            aromas of spices and the sounds of traditional music fill the air. As night falls, the city lights create a mesmerizing atmosphere that
+                            will captivate your senses.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/12.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/11.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/10.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> KUWAIT: A BLEND OF OLD AND NEW</h2>
+                        <p style="font-size: 20px;">
+                            Kuwait is a vibrant nation with a rich history and a bright future. Start your journey at the iconic Kuwait Towers, where panoramic
+                            views of the city await. The Tareq Rajab Museum showcases the rich Islamic art and culture, while the bustling avenues of Salmiya
+                            offer a modern shopping experience. Do not miss a drive along the beautiful Arabian Gulf Road, where the sea meets the skyline.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/15.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/14.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/13.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> UNITED ARAB EMIRATES: A FUSION OF CULTURES</h2>
+                        <p style="font-size: 20px;">
+                            The UAE is a dazzling blend of cultures and experiences. Drive through Dubai and experience the opulence of the Burj Khalifa and the
+                            unique architecture of the Palm Jumeirah. Visit the cultural heart of Abu Dhabi, where the magnificent Sheikh Zayed Grand Mosque
+                            stands as a testament to Islamic architecture. Each emirate offers its own unique charm, making your journey an unforgettable
+                            adventure.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/18.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/17.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/16.png"/></div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-5">
+                        <h2><i class="fa fa-location-pin mr-2" style="color: #ecd875; font-size: 60px;"/> OMAN: A HIDDEN GEM</h2>
+                        <p style="font-size: 20px;">
+                            Finally, discover Oman, a country rich in natural beauty and tradition. As you drive through Muscat, visit the stunning Sultan Qaboos
+                            Grand Mosque, and explore the vibrant Mutrah Souq. Venture into the breathtaking landscapes of the Hajar Mountains and the
+                            stunning beaches of Salalah, where crystal-clear waters invite you to unwind.
+                        </p>
+
+                        <div class="row">
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/2.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/25.png"/></div>
+                            <div class="col-md-4"><img width="500" src="/images/where_we_are/3.png"/></div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <div>
+                    <div class="offset-3"><img src="/images/where_we_are/conc.png"/></div>
+                    <div class="ml-5">
+                        <p style="font-size: 25px">
+                            Each of these eight destinations offers a unique story waiting to be discovered. With our car rental service, you can embark on a
+                            journey that takes you through the heart of these remarkable countries. Your adventure begins with the open road, leading you to
+                            experiences and memories that will last a lifetime. Let us explore together!
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <Footer/>
+
+    </body>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+import 'vue-toast-notification/dist/theme-sugar.css';
+import HeaderOne from '../components/HeaderOne.vue'
+import Footer from "../components/Footer.vue";
+import 'primevue/resources/themes/aura-light-amber/theme.css'
+
+
+onMounted(() => {
+
+})
+
+</script>
+
+<style scoped>
+#top {
+    margin-top: 7%;
+}
+</style>
