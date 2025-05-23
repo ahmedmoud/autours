f6af682bc2b20f9385a5b72c199d775ec9c9843f
Ahmed-Gouda <ahmed-gouda@maxabs-Laptop.local>
2025-05-23 11:06:02 +0300

    nav bar responsive

--- resources/js/components/HeaderOne.vue
+++ resources/js/components/HeaderOne.vue
@@ -1,37 +1,65 @@
 <template>
 
     <header style="background: #f9d602; height: 85px;">
-        <div class="row justify-content-between">
-            <a href="/" class="col-2 justify-content-center">
-                <img  src="/img/4k-logo.png" width="200" alt="logo">
-            </a>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
+            <a v-if="!user" class="nav-link col-md-2  text-black " href="/">Home</a>
+            <a v-if="!user" class="nav-link col-md-2  text-black " href="/register">Booking</a>
+            <a v-else-if="user.role !== 'customer'" class="nav-link col-md-2 text-black "
+               href="/company"><span>My&nbsp;console</span></a>
+            <a v-if="!user" class="nav-link col-md-2  text-black " href="/contact-us">Contact&nbsp;us</a>
+            <a v-if="!user" class="nav-link col-md-2  text-black " href="/about-us">About&nbsp;us</a>
+            <a v-if="user.role === 'customer'" class="col-md-2" @click="() => router.get('/my-profile')">&nbsp;&nbsp;My&nbsp;Profile</a>
+            <a v-if="user.role === 'customer'" class="col-md-2" @click="() => router.get('/logout')">&nbsp;&nbsp;Log&nbsp;Out</a>
+        </div>
 
-            <div class="topnav" id="myTopnav">
+
+        <div class="row align-items-center justify-content-between">
+            <div class="col-6 col-md-2 d-flex justify-content-start">
+                <a href="/">
+                    <img src="/img/4k-logo.png" width="200" alt="logo">
+                </a>
+            </div>
+            <div class="mobile-nav-icon col-1 col-md-2  justify-content-end">
+                <a class=" icon text-black" href="javascript:void(0);"
+                   @click="myFunction()">
+                    <i class="fa fa-bars"/>
+                </a>
+            </div>
+
+
+
+
+            <div class="topnav col-md-8" id="myTopnav">
 
                 <div class="row menu-responsive">
-
-                    <a v-if="!user" class="nav-link col-md-8  text-black "  href="/register">Manage&nbsp;Booking</a>
-                    <a v-else-if="user.role !== 'customer'" class="nav-link col-md-8 text-black "
+                    <a v-if="!user" class="nav-link col-md-2  text-black " href="/">Home</a>
+                    <a v-if="!user" class="nav-link col-md-2  text-black " href="/register">Booking</a>
+                    <a v-else-if="user.role !== 'customer'" class="nav-link col-md-2 text-black "
                        href="/company"><span>My&nbsp;console</span></a>
+                    <a v-if="!user" class="nav-link col-md-2  text-black " href="/contact-us">Contact&nbsp;us</a>
+                    <a v-if="!user" class="nav-link col-md-2  text-black " href="/about-us">About&nbsp;us</a>
 
 
-                    <a v-if="user.role === 'customer'" class="col-md-7">
+                    <a v-if="user.role === 'customer'" class="col-md-2">
                         <CDropdown togglerText="Dropdown button" class="mt-1">
                             <CDropdownToggle component="a" style="color: rgba(30, 30, 30, 1);" class="">{{
                                     user.name
                                 }}
                             </CDropdownToggle>
                             <CDropdownMenu>
-                                <CDropdownItem class="cursor-pointer  text-black " @click="() => router.get('/my-profile')"><i
+                                <CDropdownItem class="cursor-pointer  text-black "
+                                               @click="() => router.get('/my-profile')"><i
                                     class="fa fa-user "/>&nbsp;&nbsp;My&nbsp;Profile
                                 </CDropdownItem>
-                                <CDropdownItem class="cursor-pointer text-black " @click="() => router.get('/logout')"><i
-                                    class="fa fa-door-closed"/>&nbsp;&nbsp;Log&nbsp;Out
+                                <CDropdownItem class="cursor-pointer text-black " @click="() => router.get('/logout')">
+                                    <i
+                                        class="fa fa-door-closed"/>&nbsp;&nbsp;Log&nbsp;Out
                                 </CDropdownItem>
                             </CDropdownMenu>
                         </CDropdown>
                     </a>
-                    <a class="col-md-4">
+                    <a class="col-md-2 ">
                         <CDropdown togglerText="Dropdown button" class="mt-1 ">
                             <CDropdownToggle class="text-black " component="a" style="color: rgba(30, 30, 30, 1);">{{
                                     selectedCurrency
@@ -40,26 +68,15 @@
                             <CDropdownMenu>
                                 <CDropdownItem class="cursor-pointer " v-for="currency in currencies"
                                                v-on:click="changeCurrency(currency.name)">
-                                    <p class="text-black "><img style="margin-right: 25px" :src="'https://flagsapi.com/' + currency.flag + '/shiny/32.png'"> {{ currency.name }}</p>
+                                    <p class="text-black "><img style="margin-right: 25px"
+                                                                :src="'https://flagsapi.com/' + currency.flag + '/shiny/32.png'">
+                                        {{ currency.name }}</p>
                                 </CDropdownItem>
                             </CDropdownMenu>
                         </CDropdown>
                     </a>
                 </div>
-
-                <!--                <a href="#news">News</a>-->
-                <!--                <a href="#contact">Contact</a>-->
-                <!--                <a href="#about">About</a>-->
-                <a href="javascript:void(0);" class="icon " @click="myFunction()">
-                    <i class="fa fa-bars"/>
-                </a>
             </div>
-            <!--            <div class="col-10 align-self-center text-right d-flex justify-content-end topnav" id="myTopnav">-->
-
-
-            <!--           -->
-
-            <!--            </div>-->
 
         </div>
     </header>
@@ -110,13 +127,12 @@ onMounted(() => {
     getCurrencies();
 })
 const myFunction = () => {
-    var x = document.getElementById("myTopnav");
-
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
 }
 </script>
 
@@ -127,12 +143,9 @@ body {
 }
 
 .topnav {
-    overflow: hidden;
     justify-content: flex-end !important;
     align-self: center !important;
-    display: contents !important;
-    width: 50%;
-    height: 10%;
+    background-color: rgb(249, 214, 2);
 }
 
 .topnav a {
@@ -142,77 +155,86 @@ body {
     text-align: center;
     text-decoration: none;
     height: 40px;
-
     font-size: 17px;
 }
 
+
 .topnav a:hover {
-    background-color: rgba(206, 197, 7, 0.34);
+    background-color: rgba(232, 200, 4, 0.66);
     height: 40px;
 }
 
-
-.topnav .icon {
+.mobile-nav-icon {
     display: none;
-    color: black;
-    height: 0;
-    margin-top: 4%;
 }
 
-@media screen and (max-width: 1500px) {
-    .nav-icon {
-        margin-left: 50% !important;
-    }
-}
-
-@media screen and (max-width: 1000px) {
-    .topnav a {
+@media screen and (max-width: 999px) {
+    #myTopnav {
         display: none;
     }
 
-    .topnav a.icon {
-        float: right;
-        display: block;
-
-    }
-
-    .topnav p {
-        font-size: 5vw;
-    }
-
-    .topnav.responsive {
-        position: relative;
-    }
-
-    .topnav.responsive .icon {
-        position: absolute;
-        right: 8%;
-        top: 3%;
-    }
-
-    .topnav.responsive a {
-        float: right;
-        display: block;
-        text-align: right;
-    }
-
-    .menu-responsive {
-        margin-top: 8%;
+    .mobile-nav-icon {
+        display: block !important;
     }
 }
-.el-picker-panel{
+
+.el-picker-panel {
     color: #000 !important;
 }
+
 .el-date-range-picker {
     --el-datepicker-active-color: rgb(249, 214, 2) !important;
     --el-datepicker-hover-text-color: rgb(249, 214, 2) !important;
     --el-datepicker-inrange-bg-color: rgba(249, 214, 2, 0.19) !important;
 }
-    .el-date-table td.today .el-date-table-cell__text {
-        color: rgb(249, 214, 2) !important;
-        background: rgba(249, 214, 2, 0.26) !important;
-    }
-    .el-date-table td.current:not(.disabled) .el-date-table-cell__text{
-        background-color: rgb(249, 214, 2) !important;
-    }
+
+.el-date-table td.today .el-date-table-cell__text {
+    color: rgb(249, 214, 2) !important;
+    background: rgba(249, 214, 2, 0.26) !important;
+}
+
+.el-date-table td.current:not(.disabled) .el-date-table-cell__text {
+    background-color: rgb(249, 214, 2) !important;
+}
+
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #ffffff;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 60px;
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #000000;
+    display: block;
+    transition: 0.3s;
+}
+
+.sidenav a:hover {
+    color: rgba(232, 200, 4, 0.66);
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
 </style>
